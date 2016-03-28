65e7548972f4b3b4e838978c05b59a60e4c39a35
LeeKennedy <mail@leekennedy.id.au>
2016-03-28 12:55:14 +1100

    Minor adjustments

--- scripts/Validation.Report.R
+++ scripts/Validation.Report.R
@@ -7,15 +7,17 @@ library(broom)
 
 # -- insert analyte ----------------------------------
 code <- "Blahblah2"
-workbook_name <- "Validation_Workbook_2.xlsx"
-#workbook_name <- "Validation_Workbook.xlsx"
-output <- "word_document"
-doc_suffix <- ".docx"
+#workbook_name <- "Validation_Workbook_2.xlsx"
+workbook_name <- "Validation_Workbook.xlsx"
 units <- "mg/kg"
+style <- 1      # 1 = Word document, 2 = web document
+
 
 # Things to watch out for: --------------------------------
 # 1. Is workbook name correct?
-# 2. Change rounding in LOR/LOD if necessary.
+# 2. Is the report name correct?
+# 3. Have all the comments been written?
+# 4. Change rounding in LOR/LOD if necessary.
 
 # Import the data ---------------------------------------------
 linearity <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workbook_name, sep=""), sheet=1)
@@ -27,6 +29,13 @@ data_stdadd <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workb
 
 
 # Run report -------------------------------------------------
+if(style == 1) {
+        output <- "word_document"
+        doc_suffix <- ".docx"      
+}else{
+        output <- "html_document"
+        doc_suffix <- ".html"
+}
 rmarkdown::render("~/Documents/GitHub/Validation_Set/reports/VAL.RMarkdown.Report.Rmd", 
         output_format = output,
         output_file = paste(code, "-Validation Report-",Sys.Date(), doc_suffix, sep=""),
