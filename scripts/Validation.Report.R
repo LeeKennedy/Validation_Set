f329b0e2f82aded9ae0115a208c64e55c69b37b4
LeeKennedy <mail@leekennedy.id.au>
2016-03-28 11:08:30 +1100

    Polishing

--- scripts/Validation.Report.R
+++ scripts/Validation.Report.R
@@ -7,8 +7,17 @@ library(broom)
 
 # -- insert analyte ----------------------------------
 code <- "Blahblah2"
-workbook_name <- "Validation_Workbook.xlsx"
+workbook_name <- "Validation_Workbook_2.xlsx"
+#workbook_name <- "Validation_Workbook.xlsx"
+output <- "word_document"
+doc_suffix <- ".docx"
+units <- "mg/kg"
 
+# Things to watch out for: --------------------------------
+# 1. Is workbook name correct?
+# 2. Change rounding in LOR/LOD if necessary.
+
+# Import the data ---------------------------------------------
 linearity <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workbook_name, sep=""), sheet=1)
 LOR <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workbook_name, sep=""), sheet=2)
 data_anova <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workbook_name, sep=""), sheet=3)
@@ -16,9 +25,11 @@ bias <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workbook_nam
 data3 <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workbook_name, sep=""), sheet=5)
 data_stdadd <- read_excel(paste("~/Documents/GitHub/Validation_Set/data/", workbook_name, sep=""), sheet=6)
 
+
+# Run report -------------------------------------------------
 rmarkdown::render("~/Documents/GitHub/Validation_Set/reports/VAL.RMarkdown.Report.Rmd", 
-        output_format = "word_document",
-        output_file = paste(code, "-Validation Report-",Sys.Date(), ".docx", sep=""),
+        output_format = output,
+        output_file = paste(code, "-Validation Report-",Sys.Date(), doc_suffix, sep=""),
         output_dir = "~/Documents/GitHub/Validation_Set/reports")
                 
 
