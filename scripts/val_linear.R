72047142a8f2b102208d3f6e96e105d93925730f
LeeKennedy <mail@leekennedy.id.au>
2017-04-03 19:36:26 +1000

    Vitammin K1 playing

--- scripts/val_linear.R
+++ scripts/val_linear.R
@@ -1,11 +1,24 @@
-library("ProjectTemplate")
-load.project()
+
+# Clean Up environment ---------------------------------------------------
+rm(list=ls())
+
+# Packages ---------------------------------------------------------------
+library(readxl)
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(broom)
+
+
+
+# Data Input -------------------------------------------------------------
+
+linearity <- read_excel("~/Desktop/Vitamin K1/VIK101 - Validation Workbook.xlsx", 
+                        sheet = "Linearity")
+
 
 # Linearity-----------------------------------------------------------
-linearity <- Validation.Workbook.Linearity
-
-
-                
 col1 <- colnames(linearity[1])
 col2 <- colnames(linearity[2])
 colnames(linearity)[1] <- "A"
@@ -16,14 +29,17 @@ lin1 <- summary(lin)
 lin2 <- tidy(lin)
 R2 <- lin1$r.squared
 
-
+dev.off()
 
 linplot = ggplot(linearity, aes(x = A, y = B)) + 
         geom_point(size=5, shape = 21, colour = "darkgreen") + 
         geom_abline(intercept=coef(lin)[1], slope=coef(lin)[2]) + 
         stat_smooth(method = "lm", fullrange = TRUE) +
-        annotate("text", label = paste("R2 = ",round(summary(lin)$r.squared,digits=4)), x = 0.5*max(linearity[,1]), y = 0.8*max(linearity[,2])) +
-       labs(x=col1, y=col2, title = "Linearity\n") +
+        annotate("text", 
+                 label = paste("R2 = ",
+                 round(summary(lin)$r.squared,digits=4)), 
+                 x = 0.5*max(linearity[,1]), y = 0.8*max(linearity[,2])) +
+       labs(x=col1, y=col2, title = "Linearity", subtitle = "The grey zone is the 95% Confidence Interval") +
         theme_bw() +
         theme(panel.grid.major = element_line(size = 0.5, color = "grey"), 
               axis.line = element_line(size = 0.7, color = "black"), 
@@ -32,9 +48,11 @@ linplot = ggplot(linearity, aes(x = A, y = B)) +
 
 linplot
 
+
 png(filename = paste("graphs/","Linearity.png",sep=""),
     width = 1000, height = 550, units = "px", pointsize = 12)
 plot(linplot)
+
 
 lin2
 R2
@@ -44,3 +62,4 @@ R2
 coefficients(lin) # model coefficients
 confint(lin, level=0.95) # CIs for model parameters
 CI_curve <- confint(lin, level=0.95) 
+
